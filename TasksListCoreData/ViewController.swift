12e44ca000ed6b0369d71966219fea6dd0cc311d
TDB-star <tatiana.blache@gmail.com>
2021-08-18 17:35:47 +0200

    Initial Commit

---
+++ TasksListCoreData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TasksListCoreData
+//
+//  Created by Tatiana Dmitrieva on 18/08/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
